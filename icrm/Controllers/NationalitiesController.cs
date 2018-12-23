b8358909b6ca6fb31f020d7a313a74a2b8778e29
tufail <tufail.b.n@gmail.com>
2018-12-23 05:29:29 -0800

    Added Security To Controllers

--- icrm/Controllers/NationalitiesController.cs
+++ icrm/Controllers/NationalitiesController.cs
@@ -10,6 +10,7 @@ using icrm.Models;
 
 namespace icrm.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class NationalitiesController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
