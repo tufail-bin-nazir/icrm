5fcd38571eb712e00c3ecc5e4ff2caa881d5b8a3
tufail <tufail.b.n@gmail.com>
2018-12-25 22:59:56 -0800

    Registration Error Solved

--- icrm/Controllers/AccountController.cs
+++ icrm/Controllers/AccountController.cs
@@ -169,8 +169,8 @@ namespace icrm.Controllers
         public ActionResult Register()
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
-            var role = roleManager.FindByName("Department").Users.First();
+           // var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+          //  var role = roleManager.FindByName("Department").Users.First();
            
 
             ViewBag.Locationlist=db.Locations.ToList();
@@ -178,7 +178,7 @@ namespace icrm.Controllers
             ViewBag.Nationalitylist=db.Nationalities.ToList();
             ViewBag.Nationalitylist = db.Nationalities.ToList();
             ViewBag.Jobtitlelist = db.JobTitles.ToList();
-            ViewBag.Departmentlist = db.Users.Where(u => u.Roles.Any(s => s.RoleId ==role.RoleId));
+            //ViewBag.Departmentlist = db.Users.Where(u => u.Roles.Any(s => s.RoleId ==role.RoleId));
             ViewBag.CostCenterlist = db.CostCenters.ToList();
             ViewBag.Religionlist = db.Religions.ToList();
 
