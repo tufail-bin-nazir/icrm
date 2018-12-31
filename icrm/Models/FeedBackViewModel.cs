f5eead16eff70467b36c08e844ab753a0ed51b53
tufail <tufail.b.n@gmail.com>
2018-12-30 21:41:21 -0800

    created add feedback service

---
+++ icrm/Models/FeedBackViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace icrm.Models
+{
+    public class FeedBackViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Attachment { get; set; }
+
+
+
+    }
+}
--- icrm/WebApi/FeedBackController.cs
+++ icrm/WebApi/FeedBackController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -16,13 +18,13 @@ using System.Web.Http;
 namespace icrm.WebApi
 {
     [Authorize]
-    public class FeedBackController : ApiController
+    public class FeedBackApiController : ApiController
     {
         private IFeedback feedInterface;
 
         private ApplicationUserManager _userManager;
 
-        public FeedBackController()
+        public FeedBackApiController()
         {
             feedInterface = new FeedbackRepository();
         }
@@ -42,18 +44,69 @@ namespace icrm.WebApi
         }
 
         [HttpPost]
-        public IHttpActionResult PostFeedback(Feedback feedBack) {
+        public IHttpActionResult PostFeedback(FeedBackViewModel feedBackmodel) {
 
             if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
 
+          
+            //getting extension of the base 64 file
+            String ext = GetFileExtension(feedBackmodel.Attachment);
+
             var Name1 = User.Identity.Name;
-            Task<ApplicationUser> user = UserManager.FindByNameAsync(Name1);
-            feedBack.userId = user.Result.Id;
+           Task<ApplicationUser> user = UserManager.FindByNameAsync(Name1);
+           Feedback feedBack = new Feedback { title = feedBackmodel.Title, attachment = $@"{Guid.NewGuid()}." + ext, description = feedBackmodel.Description, userId = user.Result.Id };
+            string path = @"F:\Files\"+ feedBack.attachment;
+            if (!File.Exists(path))
+            {
+               FileStream fileStream =  File.Create(path);
+                fileStream.Close();
+            }
+
+            File.WriteAllBytes(path, getfile(feedBackmodel.Attachment));
+            
             feedInterface.Save(feedBack);
             return Ok(feedBack);
 
+        }
+
+        private Byte[] getfile(string stringimage) {
+            // Convert base 64 string to byte[]
+            byte[] file = Convert.FromBase64String(stringimage);
+            return file;
+           
+
+        }
+
+        private string GetFileExtension(string base64String)
+        {
+            var data = base64String.Substring(0, 5);
+
+            switch (data.ToUpper())
+            {
+                case "IVBOR":
+                    return "png";
+                case "/9J/4":
+                    return "jpg";
+                case "AAAAF":
+                    return "mp4";
+                case "JVBER":
+                    return "pdf";
+                case "AAABA":
+                    return "ico";
+                case "UMFYI":
+                    return "rar";
+                case "E1XYD":
+                    return "rtf";
+                case "U1PKC":
+                    return "txt";
+                case "MQOWM":
+                case "77U/M":
+                    return "srt";
+                default:
+                    return string.Empty;
+            }
         }
 
         //[HttpGet]
