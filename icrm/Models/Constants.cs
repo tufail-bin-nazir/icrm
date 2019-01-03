ceb5b7708a9f63cba3f6214858192afcfa1fa79f
Iram Ismail <iram.8859@gmail.com>
2019-01-03 15:03:25 +0530

    constantsChange

--- icrm/Models/Constants.cs
+++ icrm/Models/Constants.cs
@@ -7,7 +7,7 @@ namespace icrm.Models
 {
     public static class Constants
     {
-        static List<string> statusList = new List<string>(new string[] { "Open", "Closed", "Resolved" });
-       public  static string PATH = "~/Images/" ;
+        public static List<string> statusList = new List<string>(new string[] { "Open", "Closed", "Resolved" });
+       public  static string PATH = @"E:\ICRMImages\" ;
     }
 }
