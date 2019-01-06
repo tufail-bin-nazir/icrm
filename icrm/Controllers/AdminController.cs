1918276c48048f6ed4ba6d966e88cf40681c9462
tufail <tufail.b.n@gmail.com>
2019-01-05 22:54:32 -0800

    added ticket count on admin dashborard

--- icrm/Controllers/AdminController.cs
+++ icrm/Controllers/AdminController.cs
@@ -9,23 +9,29 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Diagnostics;
+using icrm.RepositoryInterface;
+using icrm.RepositoryImpl;
 
 namespace icrm.Controllers
 {
     [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
-       
+        private IFeedback feedInterface;
+
         private ApplicationUserManager _userManager;
 
         public ActionResult Dashboard()
         {
+            TicketCounts();
             return View();
         }
 
 
         public AdminController()
-        {}
+        {
+            feedInterface = new FeedbackRepository();
+        }
         public AdminController(ApplicationUserManager userManager)
         {
             this._userManager = userManager;    
@@ -126,7 +132,17 @@ namespace icrm.Controllers
             
             return View(db.Users.ToList());
         }
+
+        /****** Get Ticket Counts********/
+        public void TicketCounts()
+        {
+            ViewBag.TotalTickets = feedInterface.getAll().Count();
+            ViewBag.OpenTickets = feedInterface.getAllOpen().Count();
+            ViewBag.ClosedTickets = feedInterface.getAllClosed().Count();
+            ViewBag.ResolvedTickets = feedInterface.getAllResolved().Count();
+        }
     }
+
 
    
 }
