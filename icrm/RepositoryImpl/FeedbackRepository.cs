a56108fbc65502e9ead0934efe60824cbe2494de
Iram Ismail <iram.8859@gmail.com>
2019-01-23 15:10:37 +0530

    Comentlist

--- icrm/RepositoryImpl/FeedbackRepository.cs
+++ icrm/RepositoryImpl/FeedbackRepository.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.AspNet.Identity;
 using System.Data.SqlClient;
 using System.Data;
+using Constants = icrm.Models.Constants;
 
 namespace icrm.RepositoryImpl
 {
@@ -45,8 +46,13 @@ namespace icrm.RepositoryImpl
 
         public IPagedList<Feedback> getAllAssigned(int pageIndex, int pageSize)
         {
+            var param = new SqlParameter();
+            param.ParameterName = "@status";
+            param.SqlDbType = SqlDbType.VarChar;
+            param.SqlValue = Constants.ASSIGNED;
+
             List<Feedback> feedlist = new List<Feedback>();
-            var result = db.Feedbacks.SqlQuery("getAllAssigned").ToList();
+            var result = db.Feedbacks.SqlQuery("getAllAssigned @status",param).ToList();
             foreach (var r in result)
             {
                 feedlist.Add(r);
@@ -68,7 +74,7 @@ namespace icrm.RepositoryImpl
                         
 
         }
-        public IPagedList<Feedback> search(string d1, string d2, string status, string id,int pageIndex, int pageSize)
+        public IPagedList<Feedback> search(string d1, string d2, string status, int id,int pageIndex, int pageSize)
         {
             var param1 = new SqlParameter();
             param1.ParameterName = "@D1";
@@ -90,11 +96,16 @@ namespace icrm.RepositoryImpl
 
             var param4 = new SqlParameter();
             param4.ParameterName = "@id";
-            param4.SqlDbType = SqlDbType.VarChar;
+            param4.SqlDbType = SqlDbType.Int;
             param4.SqlValue = id;
 
+            var param5 = new SqlParameter();
+            param5.ParameterName = "@checkstatus";
+            param5.SqlDbType = SqlDbType.VarChar;
+            param5.SqlValue = Constants.RESPONDED;
+
             List<Feedback> feedlist = new List<Feedback>();
-            var result = db.Feedbacks.SqlQuery("search @D1,@D2,@Status,@id", param1, param2, param3, param4).ToList();
+            var result = db.Feedbacks.SqlQuery("search @D1,@D2,@Status,@id,@checkstatus", param1,param2,param3,param4,param5).ToList();
             foreach (var r in result) {
                 feedlist.Add(r);
             }
@@ -110,7 +121,7 @@ namespace icrm.RepositoryImpl
 
         public IEnumerable<Feedback> getAllOpen()
         {
-            return db.Feedbacks.OrderByDescending(m => m.user.Id).Where(m=>m.status=="Open").ToList();
+            return db.Feedbacks.OrderByDescending(m => m.user.Id).Where(m=>m.status==Constants.OPEN).ToList();
 
         }
         public IPagedList<Feedback> getAllOpenWithDepartment(string usrid, int pageIndex, int pageSize)
@@ -121,9 +132,13 @@ namespace icrm.RepositoryImpl
             param1.SqlDbType = SqlDbType.VarChar;
             param1.SqlValue = user.DepartmentId;
 
+            var param2 = new SqlParameter();
+            param2.ParameterName = "@status";
+            param2.SqlDbType = SqlDbType.VarChar;
+            param2.SqlValue = Constants.ASSIGNED;
 
             List<Feedback> feedlist = new List<Feedback>();
-            var result = db.Feedbacks.SqlQuery("getAllOpenWithDepart @depID", param1).ToList();
+            var result = db.Feedbacks.SqlQuery("getAllOpenWithDepart @depID,@status", param1,param2).ToList();
             foreach (var r in result)
             {
                 feedlist.Add(r);
@@ -146,8 +161,13 @@ namespace icrm.RepositoryImpl
             param2.SqlDbType = SqlDbType.VarChar;
             param2.SqlValue = usrid;
 
+            var param3 = new SqlParameter();
+            param3.ParameterName = "@status";
+            param3.SqlDbType = SqlDbType.VarChar;
+            param3.SqlValue = Constants.RESPONDED;
+
             List<Feedback> feedlist = new List<Feedback>();
-            var result = db.Feedbacks.SqlQuery("getAllRespondedWithDepart @depID,@CommentedByID", param1,param2).ToList();
+            var result = db.Feedbacks.SqlQuery("getAllRespondedWithDepart @depID,@CommentedByID,@status", param1,param2,param3).ToList();
             foreach (var r in result)
             {
                 feedlist.Add(r);
@@ -162,46 +182,40 @@ namespace icrm.RepositoryImpl
         public IPagedList<Feedback> OpenWithoutDepart(int pageIndex, int pageSize)
         {
             //return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.departmentID == null && m.response==null && m.status=="Open").ToPagedList(pageIndex, pageSize);
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.departmentID == null && m.status == "Open" && m.checkStatus=="Open").ToPagedList(pageIndex, pageSize);
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.departmentID == null  && m.checkStatus==Constants.OPEN).ToPagedList(pageIndex, pageSize);
 
         }
 
         public IPagedList<Feedback> getAllResolved(int pageIndex, int pageSize)
         {
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m =>  m.status == "Resolved").ToPagedList(pageIndex, pageSize);
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m =>  m.status == Constants.RESOLVED).ToPagedList(pageIndex, pageSize);
 
         }
         public IPagedList<Feedback> getAllOpen(int pageIndex, int pageSize)
         {
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.status == "Open").ToPagedList(pageIndex, pageSize);
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.status == Constants.OPEN).ToPagedList(pageIndex, pageSize);
 
         }
 
         public IPagedList<Feedback> getAllResponded(int pageIndex, int pageSize)
         {
-            List<Feedback> feedlist = new List<Feedback>();
-            var result = db.Feedbacks.SqlQuery("getAllResponded").ToList();
-            foreach (var r in result)
-            {
-                feedlist.Add(r);
-            }
+            
 
-            return feedlist.ToPagedList(pageIndex, pageSize);
-            // return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.departmentID != null && m.response != null && m.status == "Open").ToPagedList(pageIndex, pageSize);
+            return db.Feedbacks.OrderByDescending(m=>m.id).Where(m=>m.checkStatus==Constants.RESPONDED).ToPagedList(pageIndex, pageSize);
 
         }
 
         public IPagedList<Feedback> getAllClosed(int pageIndex, int pageSize)
         {
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.status == "Closed").ToPagedList(pageIndex, pageSize);
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.status == Constants.CLOSED).ToPagedList(pageIndex, pageSize);
         }
         public IEnumerable<Feedback> getAllClosed()
         {
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.status == "Closed").ToList();
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.status == Constants.CLOSED).ToList();
         }
         public IEnumerable<Feedback> getAllResolved()
         {
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m =>  m.status == "Resolved").ToList();
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m =>  m.status == Constants.RESOLVED).ToList();
         }
 
         public IPagedList<Feedback> searchHR(string d1, string d2, string status, int pageIndex, int pageSize)
@@ -270,8 +284,12 @@ namespace icrm.RepositoryImpl
 
         public IEnumerable<Feedback> getAllAssigned()
         {
+            var param = new SqlParameter();
+            param.ParameterName = "@status";
+            param.SqlDbType = SqlDbType.VarChar;
+            param.SqlValue = Models.Constants.ASSIGNED;
             List<Feedback> feedlist = new List<Feedback>();
-            var result = db.Feedbacks.SqlQuery("getAllAssigned").ToList();
+            var result = db.Feedbacks.SqlQuery("getAllAssigned @status",param).ToList();
             foreach (var r in result)
             {
                 feedlist.Add(r);
@@ -295,12 +313,12 @@ namespace icrm.RepositoryImpl
 
         public IEnumerable<Feedback> OpenWithoutDepart()
         {
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.departmentID == null && m.status == "Open").ToList();
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.departmentID == null && m.checkStatus == Constants.OPEN).ToList();
         }
 
         public IPagedList<Feedback> getAllRejected(int pageIndex, int pageSize)
         {
-            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.checkStatus == "Rejected").ToPagedList(pageIndex,pageSize);
+            return db.Feedbacks.OrderByDescending(m => m.id).Where(m => m.checkStatus == Constants.REJECTED).ToPagedList(pageIndex,pageSize);
 
         }
 
@@ -327,6 +345,11 @@ namespace icrm.RepositoryImpl
             }
             return feedlist;
 
+        }
+
+        public List<Comments> getCOmments(string id)
+        {
+            return db.comments.Where(m => m.feedbackId == id).ToList();
         }
     }
 }
