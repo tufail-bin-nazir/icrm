fb62b20599e4f836805b1ec92688a6c170178792
tufail <tufail.b.n@gmail.com>
2019-01-23 05:52:52 -0800

    new  user data uploader

---
+++ icrm/Models/CultureAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+using System.Web.Mvc;
+
+namespace icrm.Models
+{
+    public class CultureAttribute : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var culture = "";
+            if (filterContext.HttpContext.Session["culture"] as String != null)
+            {
+                culture = filterContext.HttpContext.Session["culture"] as String;
+            }
+            else
+            {
+                culture = "en-US";
+            }
+            //Retreive culture from GET
+            // string currentCulture = filterContext.HttpContext.Request.QueryString["culture"];
+
+            // Also, you can retreive culture from Cookie like this :
+           // string currentCulture = filterContext.HttpContext.Session["culture"] as String;
+
+            // Set culture
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(culture);
+        }
+    }
+}
