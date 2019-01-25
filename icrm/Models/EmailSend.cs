f503d0e15eea2a9754a4513e8f8b329a3c235809
Iram Ismail <iram.8859@gmail.com>
2019-01-25 15:51:39 +0530

    emal cc

--- icrm/Models/EmailSend.cs
+++ icrm/Models/EmailSend.cs
@@ -1,35 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Web;
+using System.Data.Entity.Core.Objects;
+using System.Net.Http;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using Constants = icrm.Models.Constants;
+using Comments = icrm.Models.Comments;
+using System.Net;
+using System.Net.Mime;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace icrm.Models
 {
     public class EmailSend
     {
 
-        public EmailSend() {
+        public EmailSend()
+        {
 
 
         }
 
 
-        public async System.Threading.Tasks.Task sendEmailAsync()
-        {  var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
-                var message = new MailMessage();
-                message.To.Add(new MailAddress("iram.8859@gmail.com")); //replace with valid value
-                message.Subject = "Your email subject";
-                message.Body = string.Format(body, "tufail.b.n@gmail.com", "tufail.b.n@gmail.com", "suit");
-                message.IsBodyHtml = true;
-                using (var smtp = new SmtpClient())
-                {
-                    await smtp.SendMailAsync(message);
-
-                }
+        public async Task sendEmailAsync(string emails,string body)
+        {
+            string[] Multi = emails.Split(',');
+            foreach (string email in Multi)
+            {
+                Debug.WriteLine(email + "hhhhhhhhhhhhhhgsssssssss");
             }
+
+            string b = body;
+
+
+            EmailSend e = new EmailSend();
+            var message = new MailMessage();
+            foreach (string email in Multi)
+            {
+                message.To.Add(new MailAddress(email));
+            }
+             //message.To.Add(new MailAddress("iram.8859@gmail.com")); //replace with valid value
+            message.Subject = "Your email subject";
+            message.Body = string.Format(b, "tufail.b.n@gmail.com", "tufail.b.n@gmail.com", "suit");
+
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                await smtp.SendMailAsync(message);
+
+            }
+
         }
+
+
+    }
     }
 
 
