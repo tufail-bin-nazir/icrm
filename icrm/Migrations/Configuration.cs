4ca9dc044e3d0fe08e78bc5cd16f1a9a6c20ed53
tufail <tufail.b.n@gmail.com>
2019-02-08 02:43:19 -0800

    disbaled auto migration

--- icrm/Migrations/Configuration.cs
+++ icrm/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace icrm.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(icrm.Models.ApplicationDbContext context)
