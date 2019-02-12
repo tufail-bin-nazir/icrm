4eb5ed636558d272d5ac7d3c53b0690ff5059f7f
Iram Ismail <iram.8859@gmail.com>
2019-02-12 15:06:39 +0530

    catt

--- icrm/Models/CategoryViewModel.cs
+++ icrm/Models/CategoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,6 @@ namespace icrm.Models
     public class CategoryViewModel
     {
         public Category Category { get; set; }
-        public IEnumerable<Category> Categories { get; set; }
+        public IPagedList<Category> Categories { get; set; }
     }
 }
