2f4f08ff17ab4c221b28b628a43daaeaf6bcb7c2
tufail <tufail.b.n@gmail.com>
2019-02-18 05:17:55 -0800

    schedule channges

--- icrm/Models/ScheduleforCriticalEscalation.cs
+++ icrm/Models/ScheduleforCriticalEscalation.cs
@@ -45,9 +45,16 @@ namespace icrm.Models
                     foreach (Feedback f in level1query) {
                         
                         f.escalationlevel = "level1";
-                        sendEmailAsync(f, db.Users.Find(getEmailOfUser(f).secondEscalationUserId).bussinessEmail);
+                        if (f.department.name.Equals(Constants.OPERATIONS))
+                        {
+                            sendEmailAsync(f, getOperationsEscalationUser(f.user.CostCenter.Id).secondEscalationUser.bussinessEmail);
+                        }
+                        else {
+                            sendEmailAsync(f, db.Users.Find(getEmailOfUser(f).secondEscalationUserId).bussinessEmail);
+                        }
                         db.Feedbacks.Add(f);
                         db.Entry(f).State = System.Data.Entity.EntityState.Modified;
+
                     }
                     db.SaveChanges();
 
@@ -60,7 +67,14 @@ namespace icrm.Models
                     foreach (Feedback f in level2query)
                     {
                         f.escalationlevel = "level2";
-                        sendEmailAsync(f, db.Users.Find(getEmailOfUser(f).thirdEscalationUserId).bussinessEmail);
+                        if (f.department.name.Equals(Constants.OPERATIONS))
+                        {
+                            sendEmailAsync(f, getOperationsEscalationUser(f.user.CostCenter.Id).secondEscalationUser.bussinessEmail);
+                        }
+                        else {
+                            sendEmailAsync(f, db.Users.Find(getEmailOfUser(f).thirdEscalationUserId).bussinessEmail);
+                        }
+                        
                         db.Feedbacks.Add(f);
                         db.Entry(f).State = System.Data.Entity.EntityState.Modified;
                     }
@@ -140,7 +154,14 @@ namespace icrm.Models
 
         }
 
-        
+        public EscalationUser getOperationsEscalationUser(int? costCenterId)
+        {
+            EscalationUser escUser = db.EscalationUsers.Where(m => m.CostCenterId == costCenterId).FirstOrDefault();
+
+            return escUser;
+        }
+
+
     }
 
 
