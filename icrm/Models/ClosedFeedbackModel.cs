2f91e3483d9f5c6fbd2ff2b7963a97328eec61e6
wajahat Nabi <wajahatnabi90@gmail.com>
2019-02-19 15:27:55 +0530

    ohk

---
+++ icrm/Models/ClosedFeedbackModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace icrm.Models
+{
+    public class ClosedFeedbackModel
+    {
+
+        public string status { get; set; }
+        public string satisfaction { get; set; }
+    }
+}
