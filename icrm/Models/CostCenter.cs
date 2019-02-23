cbdfbb3981a4547163e991826e7151f861909d4d
tufail <tufail.b.n@gmail.com>
2019-02-23 01:10:54 -0800

    added dash

--- icrm/Models/CostCenter.cs
+++ icrm/Models/CostCenter.cs
@@ -10,6 +10,6 @@ namespace icrm.Models
         public int Id { get; set; }
         public string CostCenterCode { get; set; }
         public String name { get; set; }
-        public String costCenterDisplay { get { return string.Format("{0} {1}", name, CostCenterCode); } }
+        public String costCenterDisplay { get { return string.Format("{0}-{1}", name, CostCenterCode); } }
     }
 }
